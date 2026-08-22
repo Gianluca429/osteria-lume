6e9df840d3aeef93dde21281936a9202cbd9d4bc
Gianluca429 <lukas.guarino93@gmail.com>
2026-08-22 13:32:53 +0200

    Complete homepage sections and gallery

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,13 @@ import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
 import { Intro } from './components/intro/intro';
 import { MenuPreview } from './components/menu-preview/menu-preview';
+import { Philosophy } from './components/philosophy/philosophy';
+import { Gallery } from './components/gallery/gallery';
+import { Reservation } from './components/reservation/reservation';
+import { Footer } from './components/footer/footer';
 
 @Component({
-  imports: [Navbar, Hero, Intro, MenuPreview],
+  imports: [Navbar, Hero, Intro, MenuPreview, Philosophy, Gallery, Reservation, Footer],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
---
+++ src/app/components/philosophy/philosophy.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-philosophy',
+  styleUrl: './philosophy.scss',
+  templateUrl: './philosophy.html',
+})
+export class Philosophy {}
